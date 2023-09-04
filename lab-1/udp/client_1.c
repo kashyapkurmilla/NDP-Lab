0fc470a8f798f7db1c2a5c5ba060ad64d5481338
Kashyap Kurmilla <124886947+kashyapkurmilla@users.noreply.github.com>
2023-09-04 23:48:39 +0530

    lab-1

--- lab-1/udp/client_1.c
+++ lab-1/udp/client_1.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 
 #define myport 9090
+#define maxsize 1024
 
 int main() {
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -23,28 +24,26 @@ int main() {
     memset(server_addr.sin_zero, '\0', sizeof(server_addr.sin_zero));
 
     int size = sizeof(struct sockaddr);
-    connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    printf("Socket Connected\n");
 
-    char buff[99];
+    char buff[maxsize];
     int len, a = 0, e = 0, i = 0, o = 0, u = 0;
-    char result[20];
+    char result[maxsize];
 
     while (1) {
         printf("Enter a string (or 'halt' to exit): ");
-        scanf("%s", buff);
+        fgets(buff, sizeof(buff), stdin);
+        buff[strcspn(buff, "\n")] = '\0'; // Remove trailing newline
 
         if (strcmp(buff, "halt") == 0) {
-            sendto(sockfd, buff, sizeof(buff), 0, (struct sockaddr *)&server_addr, size);
+            sendto(sockfd, buff, strlen(buff) + 1, 0, (struct sockaddr *)&server_addr, size);
             printf("Exiting...\n");
             close(sockfd);
             exit(0);
         }
 
-        sendto(sockfd, buff, sizeof(buff), 0, (struct sockaddr *)&server_addr, size);
+        sendto(sockfd, buff, strlen(buff) + 1, 0, (struct sockaddr *)&server_addr, size);
 
-        // Receive the result and other data from the server
-        recvfrom(sockfd, result, sizeof(result), 0, (struct sockaddr *)&server_addr, &size);
+        recvfrom(sockfd, result, sizeof(result), 0, (struct sockaddr *)&server_addr, &size);//refer terminal manual
         recvfrom(sockfd, &len, sizeof(len), 0, (struct sockaddr *)&server_addr, &size);
         recvfrom(sockfd, &a, sizeof(a), 0, (struct sockaddr *)&server_addr, &size);
         recvfrom(sockfd, &e, sizeof(e), 0, (struct sockaddr *)&server_addr, &size);
